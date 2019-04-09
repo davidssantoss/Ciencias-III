a534263925db6d7c2f5d32b0b52b44abac146b36
davidssantoss <davidsteven.santos9@gmail.com>
2019-04-08 22:11:11 -0500

    Organiza según autor y luego titulo

--- Biblioteca/Libros.py
+++ Biblioteca/Libros.py
@@ -4,97 +4,56 @@ from operator import itemgetter, attrgetter
 cola = Cola()
 
 def mostrarRes(lista):
-    print("Libros: ")
+    print("Libros en cola: ")
     for item in lista:
         print("--> {}".format(item))
     print("")
 
 def getInfoFile():    
-    lista = [x.split(", ") for x in open("./Libros.txt").readlines()]
+    lista = [x.split(", ") for x in open("./Libros.txt").readlines()]    
     for i in range(len(lista)):
         miLista = lista[i]
         for j in range(len(miLista)):
-            lst = [j, j ,j]            
+            lst = [j, j ,j, j]            
         lst[0] = miLista[0]
-        lst[1] = miLista[1]        
-        lt2 = miLista[2].split(" \n")
+        lst[1] = miLista[1]
+        lt = miLista[2]
+        lt2 = miLista[3].split(" \n")
         autor = lst[0]
         titulo = lst[1]
-        tema = lt2[0]        
-        libro = {
-            'Titulo': titulo,
-            'Autor': autor,
-            'Tema': tema
-        }        
-        cola.encolar(libro)                    
-        clave = libro.keys()        
-        valor = libro.values()
-        cantidad_datos = libro.items()
-        """
-        for clave in cola.items:
-            print(clave[1])
-            #if(clave == 'Titulo'):
-                #print (clave + valor)
-        #ordena por valores
-            
-        for key, value in sorted(libro.iteritems(), key = itemgetter(1), reverse = False):
-            print(key, value)
-        """
-def buscar(key, value):
-    cola_res = Cola()
+        tema = lt
+        paginas = lt2[0]
+        listaTotal = (autor, titulo, tema, paginas)        
+        cola.encolar(listaTotal)
+        print(listaTotal)
+    print("\n *** ordenar por autor y despues por titulo ***\n")    
+    busqueda = raw_input("Digite el nombre del autor: ")
+    cola_ord = Cola()
     for item in cola.items:
-        if value == item[key]:
-            cola_res.encolar(item)    
-    putInfoFile(cola_res.items)
-    mostrarRes(cola_res.items)
+        if busqueda == item[0]:            
+            cola_ord.encolar(item)
+    mostrarRes(cola_ord.items)
+    burbuja(cola_ord.items)
+    organizado = cola_ord.items
+    mostrarRes(organizado)  
+        
+def burbuja(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
 
 def putInfoFile(lista):    
     archivo = open("./librosOrdenados.txt", "w")
     archivo.write(str(lista))
     archivo.close()
 
-def ordenar(llave, value):
-    cola_ord = Cola()
-    for item in cola.items:
-        if value == item[llave]:
-            cola_ord.encolar(item)            
-            #sorted(cola_ord.items, key=attrgetter('Titulo'))
-    mostrarRes(cola_ord.items)
-
-def sortedDictValues(adict):
-    items = adict.items()
-    items.sort()
-    return [value for key, value in items]
 
 def run():
-    getInfoFile()
-    print("*** Ordenar por cola los libros ***")
-    tipos = "1) Consultar libro por autor \n2) Consultar libro por tema \n3) Consultar libro por titulo \n4) Mostrar libros \n5)Terminar"
-    
-    opcion = int(raw_input("Seleccione el numero de la opcion a consultar: \n{}:\n".format(tipos)))
-        
-    if opcion == 1:
-        print("\t\t Consultar libros por autor")
-        busqueda = raw_input ("Ingrese el nombre del autor del libro: ")
-        buscar('Autor', busqueda)
-        print("ordenar por titulo")
-        ordenar('Autor', busqueda)
-        
-    elif opcion == 2:
-        print("\t\t Consultar libros por tema")
-        busqueda = raw_input ("Ingrese el tema del libro: ")
-        buscar('Tema', busqueda)
-    elif opcion == 3:
-        print("\t\t Consultar libros por titulo")
-        busqueda = raw_input ("Ingrese el titulo del libro: ")
-        buscar('Titulo', busqueda)
-    elif opcion == 4:            
-        mostrarRes(cola.items)
-    elif opcion == 5:
-        exit()
-    else:
-        print("Opcion no valida, vuelva a digitar una opcion")
-        
+    print("\t ***Lista de libros del archivo***\n")
+    getInfoFile()        
     
 if __name__ == '__main__':
     run()
