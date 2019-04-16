456c80f22a4474ab119d730419a9b890cdafc3d2
davidssantoss <davidsteven.santos9@gmail.com>
2019-04-16 13:21:18 -0500

    Evalua expresiones
    evalua las expresiones basicas + - * /

--- Expresiones/getFile.py
+++ Expresiones/getFile.py
@@ -1,9 +1,43 @@
+from pila import *
+from arbol import *
+
+pila = Pila()
 def run():
     archivo = open("./expresiones_in.txt", 'r')
-    getInfo = archivo.read().splitlines()
-    print(getInfo)
+    for linea in archivo.readlines():
+        x = linea.split("\n")
+        convertir(x[0].split(" "), pila)
+        resultado = evaluar(pila.desapilar())
+        print(str(resultado))
+        writeResult(str(resultado)+"\n")
+    archivo.close()
 
+    
+def writeResult(res):
+    archivo = open("./Expresiones_out.txt", "a")
+    archivo.write(res)
+    archivo.close()
 
+def convertir(lista, pila):
+    if lista != []:
+        if lista[0] in "+-*/":
+            nodo_der = pila.desapilar()
+            nodo_izq = pila.desapilar()
+            pila.apilar(Nodo(lista[0],nodo_izq,nodo_der))
+        else:
+            pila.apilar(Nodo(lista[0]))
+        return convertir(lista[1:],pila)
+
+def evaluar(arbol):
+    if arbol.valor == "+":
+        return evaluar(arbol.izq) + evaluar(arbol.der)
+    if arbol.valor == "-":
+        return evaluar(arbol.izq) - evaluar(arbol.der)
+    if arbol.valor == "/":
+        return evaluar(arbol.izq) / evaluar(arbol.der)
+    if arbol.valor == "*":
+        return evaluar(arbol.izq) * evaluar(arbol.der)
+    return int(arbol.valor)
 
 if __name__ == '__main__':
     run()
--- Expresiones/pila.py
+++ Expresiones/pila.py
@@ -1,10 +1,10 @@
 class Pila:
     """ Representa una pila con operaciones de apilar, desapilar y
-        verificar si está vacía. """
+        verificar si esta vacia. """
  
     def __init__(self):
-        """ Crea una pila vacía. """
-        # La pila vacía se representa con una lista vacía
+        """ Crea una pila vacia. """
+        # La pila vacia se representa con una lista vacia
         self.items=[]
 
     def apilar(self, x):
@@ -14,12 +14,12 @@ class Pila:
 
     def desapilar(self):
         """ Devuelve el elemento tope y lo elimina de la pila.
-            Si la pila está vacía levanta una excepción. """
+            Si la pila esta vacia levanta una excepcion. """
         try:
             return self.items.pop()
         except IndexError:
-            raise ValueError("La pila está vacía")
+            raise ValueError("La pila esta vacia")
 
     def es_vacia(self):
-        """ Devuelve True si la lista está vacía, False si no. """
+        """ Devuelve True si la lista esta vacia, False si no. """
         return self.items == []
