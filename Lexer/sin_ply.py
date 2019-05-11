d6077897aa35f48b25b40f0418492a8737f75258
davidssantoss <davidsteven.santos9@gmail.com>
2019-05-11 12:51:47 -0500

    primer ejer Hecho

---
+++ Lexer/sin_ply.py
@@ -0,0 +1,37 @@
+import re
+"""
+@author: David S
+"""
+
+def validarExpresion():
+
+    str = "5 5 + 9 * a"
+    y = re.split("\s", str)
+
+    for lista in y:
+        buscarDec(lista)
+        buscarVar(lista)
+        buscarOpe(lista)
+
+def buscarDec(lista):
+    values = "[0-9]"
+    v = re.findall(values, lista)
+    if (v):
+        print ("El siguiente digito %s " %v + "es de tipo value")
+    return v
+
+def buscarVar(lista):
+    variable = "[a-z]"
+    var = re.findall(variable, lista)
+    if (var):
+        print ("El siguiente digito %s " %var + "es de tipo variable")
+    return var
+
+def buscarOpe(lista):
+    operators = "[+,-,*,/]"
+    op = re.findall(operators, lista)
+    if (op):
+        print ("El siguiente digito %s " %op + "es de tipo operator")
+    return op
+
+validarExpresion()
