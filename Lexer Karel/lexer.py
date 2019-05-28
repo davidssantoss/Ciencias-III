a9c04c120b1981fb62c06a1f3038143dc5e92371
davidssantoss <davidsteven.santos9@gmail.com>
2019-05-27 23:10:00 -0500

    definiendo tokens

--- Lexer Karel/lexer.py
+++ Lexer Karel/lexer.py
@@ -25,6 +25,42 @@ Letra
 EOF
 @Link de documentacion de pascal para el lexer: https://omegaup.com/karel.js/manual/KarelSyntax_es.html#prod21
 """
+declaracionPrograma = ('PROGRAMA',
+    'iniciar-programa',
+    'inicia-ejecucion',
+    'termina-ejecucion',
+    'finalizar-programa',)
+declaracionProcedimiento = ('define-nueva-instruccion',
+    'como',)
+expresion = ('LLAMADA','SI', 'MIENTRAS', 'PARA',
+    'apagate',
+    'gira-izquierda',
+    'avanza',
+    'coge-zumbador',
+    'deja-zumbador',
+    'sal-de-instruccion',
+    'inicio',
+    'fin', )
+funcionBooleana = ('frente-libre',
+    'frente-bloqueado',
+    'izquierda-libre',
+    'izquierda-bloqueada',
+    'derecha-libre',
+    'derecha-bloqueada',
+    'junto-a-zumbador',
+    'no-junto-a-zumbador',
+    'algun-zumbador-en-la-mochila',
+    'ningun-zumbador-en-la-mochila',
+    'orientado-al-norte',
+    'orientado-al-sur',
+    'orientado-al-este',
+    'orientado-al-oeste',
+    'no-orientado-al-norte',
+    'no-orientado-al-sur',
+    'no-orientado-al-este',
+    'no-orientado-al-oeste',)
+
+tokens = declaracionPrograma + declaracionProcedimiento + expresion + funcionBooleana + ('IDENTIFICADOR')
 
 def t_error(t):
     print("Illegal character '%s'" % t.value[0])
